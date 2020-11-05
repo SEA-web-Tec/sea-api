150dfbd8c48868cb22d1da40103d6896e1ed19f8
Carlos de Jesús Benoit Mendívil <cdjbm03@gmail.com>
2020-11-04 22:08:05 -0700

    migration run successfully

--- database/migrations/1_usuario_schema.js
+++ database/migrations/1_usuario_schema.js
@@ -7,7 +7,7 @@ class UsuarioSchema extends Schema {
   up () {
     this.create('usuarios', (table) => {
       table.increments('id').primary()
-      table.integer('NumeroEconomico', 11).notNullable().unique().reference('NumeroEconomico')
+      table.integer('NumeroEconomico', 11).notNullable().unique()
       table.string('Nombre', 50).notNullable()
       table.string('ApellidoPaterno', 50).notNullable()
       table.string('ApellidoMaterno', 50).notNullable()
@@ -15,13 +15,12 @@ class UsuarioSchema extends Schema {
       table.string('CURP', 18).notNullable()
       table.string('Correo', 100).notNullable()
       table.string('CedulaProfesional', 100).notNullable()
-      table.string('DepartamentoAcademico', 100).notNullable().reference('DepartamentoAcademico')
+      table.string('DepartamentoAcademico', 100).notNullable()
       table.enu('Sexo', ['MASCULINO','FEMENINO']).notNullable().defaultTo('MASCULINO')
       table.enu('Estudios', ['PENDIENTE','PRIMARIA','SECUNDARIA','BACHILLERATO','TECNICO','PASANTE','LICENCIATURA','ESPECIALIDAD','MAESTRIA','DOCTORADO']).notNullable().defaultTo('PENDIENTE')
       table.integer('user_type', 1).notNullable()
       table.string('contrasenia', 60).notNullable()
       table.timestamps()
-      table.reference()
     })
   }
 
