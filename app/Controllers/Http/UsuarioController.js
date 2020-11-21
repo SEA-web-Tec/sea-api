1d3e599c2959582b3e495330b8694be152fbad23
Carlos de Jesús Benoit Mendívil <cdjbm03@gmail.com>
2020-11-20 21:13:14 -0700

    reduce login

--- app/Controllers/Http/UsuarioController.js
+++ app/Controllers/Http/UsuarioController.js
@@ -19,7 +19,16 @@ class UsuarioController {
             var res = {}
             const token = await auth.generate(user)
             res.token = token.token
-            res.user = user
+            res.user = {
+              id: user.id,
+              nombres: user.nombre,
+              apellidoPaterno: user.apellidoPaterno,
+              apellidoMaterno: user.apellidoMaterno,
+              correo: user.correo,
+              fotoPerfil: user.fotoPerfil,
+              fotoPortada: user.fotoPortada,
+              sexo: user.sexo
+            }
             return response.status(200).json(res)
         }
         return response.status(401).json({
