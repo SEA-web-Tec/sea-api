f3f71f1a40eb9148e9247a653c23adf1fb053980
Edson David Puente Guerrero <edpg_@hotmail.com>
2020-12-11 23:47:02 -0700

    ID ya PDF

--- app/Controllers/Http/InstrumentaciondidacticaController.js
+++ app/Controllers/Http/InstrumentaciondidacticaController.js
@@ -10,65 +10,75 @@ const Database = use("Database");
 
 class InstrumentaciondidacticaController {
   //solo el evaluador buscar todas las intrumentaciones Entregadas
-  async index({ response }) {
-    const Database = use("Database");
-    const busqueda = await Database.table("instrumentaciondidacticas")
-      .select(
-        "instrumentaciondidacticas.id",
-        "materias.nombre as materiaNombre",
-        "usuarios.nombres",
-        "apellidoPaterno",
-        "apellidoMaterno",
-        "grupos.grupo"
-      )
-      .innerJoin(
-        "usuarios",
-        "instrumentaciondidacticas.usuario_id",
-        "usuarios.id"
-      )
-      .innerJoin("grupos", "instrumentaciondidacticas.grupo_id", "grupos.id")
-      .innerJoin("materias", "grupos.materia_id", "materias.id")
-      .where("estado", "Entregada");
-    return response.status(201).json(busqueda);
+  async index({ response, auth }) {
+    const usuario = auth.getUser();
+    if (usuario.userType === 1) {
+      const Database = use("Database");
+      const busqueda = await Database.table("instrumentaciondidacticas")
+        .select(
+          "instrumentaciondidacticas.id",
+          "materias.nombre as materiaNombre",
+          "usuarios.nombres",
+          "apellidoPaterno",
+          "apellidoMaterno",
+          "grupos.grupo"
+        )
+        .innerJoin(
+          "usuarios",
+          "instrumentaciondidacticas.usuario_id",
+          "usuarios.id"
+        )
+        .innerJoin("grupos", "instrumentaciondidacticas.grupo_id", "grupos.id")
+        .innerJoin("materias", "grupos.materia_id", "materias.id")
+        .where("estado", "Entregada");
+      return response.status(201).json(busqueda);
+    }
   }
 
   //buscarIntrumentacion Seleccionada
-  async buscarIntrumentacion({ response, params }) {
-    const intrumentacion = await Instrumentaciondidactica.query()
-      .where("id", params.id_ins)
-      .first();
-    const grupo = await Grupo.query()
-      .where("id", intrumentacion.grupo_id)
-      .fetch();
-    const materia = await Materia.query()
-      .where("id", grupo.toJSON()[0].materia_id)
-      .first();
-    const unidades = await Instrumentaciondidacticaunidad.query()
-      .where("id_ins", params.id_ins)
-      .fetch();
-    return response.json({
-      intrumentacion: intrumentacion,
-      unidades: unidades,
-      no_unidades: materia.unidades,
-    });
+  async buscarIntrumentacion({ response, params, auth }) {
+    const usuario = auth.getUser();
+    if (usuario.userType === 1) {
+      const intrumentacion = await Instrumentaciondidactica.query()
+        .where("id", params.id_ins)
+        .first();
+      const grupo = await Grupo.query()
+        .where("id", intrumentacion.grupo_id)
+        .fetch();
+      const materia = await Materia.query()
+        .where("id", grupo.toJSON()[0].materia_id)
+        .first();
+      const unidades = await Instrumentaciondidacticaunidad.query()
+        .where("id_ins", params.id_ins)
+        .fetch();
+      return response.json({
+        intrumentacion: intrumentacion,
+        unidades: unidades,
+        no_unidades: materia.unidades,
+      });
+    }
   }
 
   //solo el evaluador
-  async evaluar({ request, response }) {
-    const info = request.all();
-    console.log(info);
-    let estado = "Aprobada";
-    if (info.comentario != null) {
-      estado = "Rechazada";
+  async evaluar({ request, response, auth }) {
+    const usuario = auth.getUser();
+    if (usuario.userType === 1) {
+      const info = request.all();
+      console.log(info);
+      let estado = "Aprobada";
+      if (info.comentario != null) {
+        estado = "Rechazada";
+      }
+      console.log(estado);
+      await Instrumentaciondidactica.query().where("id", info.id_ins).update({
+        estado: estado,
+        comentario: info.comentario,
+        id_evaluador: usuario.id,
+      });
+      return response.json({
+        mensaje: "Intrumentacion evaluada correctamente",
+      });
     }
-    console.log(estado);
-    await Instrumentaciondidactica.query().where("id", info.id_ins).update({
-      estado: estado,
-      comentario: info.comentario,
-    });
-    return response.json({
-      mensaje: "Intrumentacion evaluada correctamente",
-    });
   }
 
   async store({ request, response }) {
--- start/routes.js
+++ start/routes.js
@@ -24,65 +24,67 @@ Route.group(() => {
   Route.post("materias/crear", "MateriaController.store").middleware("auth");
 
   //Temario
-  Route.post("temario/crear", "TemarioController.store");
-  Route.get("temario/consulta", "TemarioController.index");
+  Route.post("temario/crear", "TemarioController.store").middleware("auth");
+  Route.get("temario/consulta", "TemarioController.index").middleware("auth");
 
   //Indicadores de alcence de ID
-  Route.post(
-    "indicadoresalcance/crear",
-    "IndicadoresalcanceController.store"
-  ).middleware("auth");
+  Route.post("indicadoresalcance/crear", "IndicadoresalcanceController.store")
+    .middleware("auth")
+    .middleware("auth");
   Route.get(
     "indicadoresalcance/consulta/",
     "IndicadoresalcanceController.index"
-  );
-  Route.put("indicadoresalcance/editar", "IndicadoresalcanceController.update");
+  ).middleware("auth");
+  Route.put(
+    "indicadoresalcance/editar",
+    "IndicadoresalcanceController.update"
+  ).middleware("auth");
 
   //ID para crear, consultar y buscar completos
   Route.post(
     "instrumentaciondidactica/crear",
     "InstrumentaciondidacticaController.store"
-  );
+  ).middleware("auth");
   //Multiple
   Route.get(
     "instrumentaciondidactica/intrumentacion_completa",
     "InstrumentaciondidacticaController.intrumentacion_completa"
-  );
+  ).middleware("auth");
 
   //ID solo para el evaluador
   Route.get(
     "instrumentaciondidactica/buscar_intrumentacion/:id_ins",
     "InstrumentaciondidacticaController.buscarIntrumentacion"
-  );
+  ).middleware("auth");
   Route.get(
     "instrumentaciondidactica/consulta_intrumentaciones",
     "InstrumentaciondidacticaController.index"
-  );
+  ).middleware("auth");
   Route.post(
     "instrumentaciondidactica/evaluar",
     "InstrumentaciondidacticaController.evaluar"
-  );
+  ).middleware("auth");
 
   //ID para crear, consultar y buscar completos
   Route.post(
     "instrumentaciondidacticaunidad/crear",
     "InstrumentaciondidacticaunidadController.store"
-  );
+  ).middleware("auth");
   Route.get(
     "instrumentaciondidacticaunidad/consulta",
     "InstrumentaciondidacticaunidadController.index"
-  );
+  ).middleware("auth");
   Route.get(
     "instrumentaciondidacticaunidad/editar",
     "InstrumentaciondidacticaunidadController.update"
-  );
+  ).middleware("auth");
 
   Route.post(
     "evidenciasaprendizaje/crear",
     "EvidenciasaprendizajeController.store"
-  );
+  ).middleware("auth");
   Route.get(
     "evidenciasaprendizaje/consulta/:id_ins",
     "EvidenciasaprendizajeController.index"
-  );
+  ).middleware("auth");
 }).prefix("api/v1/");
